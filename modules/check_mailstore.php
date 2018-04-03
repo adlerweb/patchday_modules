0b6fed120dd946d0e1e7806739b5eb3f5af04acc
Florian Knodt <git@adlerweb.info>
2018-04-03 14:33:17 +0200

    mailstore: Cleanup

--- modules/check_mailstore.php
+++ modules/check_mailstore.php
@@ -11,40 +11,16 @@ class PD_check_mailstore extends PD_check {
 
     public function check() {
 	$windows = file_get_contents('https://my.mailstore.com/Downloads?lang=de');
-	#ä$windows = file_get_contents('test');
 	$windows = 'obj = ['.$this->strcrop($windows, 'obj = [', '</script>');
 
-	#var_dump($windows);
-	#var_dump($windows);
-	#$windows = json_decode($windows);
-	#var_dump($windows);
-
-	preg_match_all('/\s+"version": "([\d\.]+)",.+?\s+"product": "Server"/s', $windows, $match);
-	#print_r($match);
+	preg_match_all('/\{\s+"version": "([\d\.]+)",.+?\s+"product": "Server"/s', $windows, $match);
 
 	$srv = 0;
 	for($i=0; $i<count($match[1]); $i++) {
-		if($match[1][$i] > $srv) $srv = $match[1][$i];
+        if($match[1][$i] > $srv) $srv = $match[1][$i];
 	}
 	$version = $srv;
-	#var_dump($srv);
 
-	#exit(0);
-
-	//Clean until we get parseable JSON
-	#$windows = substr(trim($windows), 10, strlen($windows)-11);
-	
-	#$windows = json_decode($windows);
-	
-#	$version=false;
-	
-	#for($i=count($windows)-1; $i>0; $i--) {
-#		if(isset($windows[$i]->product) && $windows[$i]->product == 'Server') {
-#			$version = $windows[$i]->version;
-#			break;
-#		}
-#	}
-	
 	$download32 = 'https://my.mailstore.com/Downloads?lang=de';
         if(!$version || !$download32 || $version <= 0) {
             $this->fail('mailstore Windows');
@@ -56,7 +32,5 @@ class PD_check_mailstore extends PD_check {
 
 $check = new PD_check_mailstore;
 $check->check();
-
-
 
 ?>
