2b4605addd600a0b1789528a48f06a4409e714c3
Florian Knodt <git@adlerweb.info>
2018-04-03 14:51:06 +0200

    GCC: Cleanup

--- modules/check_gcc.php
+++ modules/check_gcc.php
@@ -11,35 +11,27 @@ class PD_check_gcc extends PD_check {
 
     public function check() {
         $html = file_get_contents('http://gcc.gnu.org/');
-	#$html = file_get_contents('test.html');
-	$html = $this->strcrop($html, 'Releases</h2>', '</dl>');
-        #$version_st = $this->preg_match_singlearg('!GCC ([\d\.]+)</a></span>.\s+?\(<a href="gcc-[\d\.]+/changes.html">changes</a>\).</dt>!s', $html);
-        #$version_os = $this->preg_match_singlearg('!GCC ([\d\.]+)</a></span>.\s+?\(<a href="gcc-[\d\.]+/changes.html">changes</a>\) \(oldest!s', $html);
-        #$version_dv = $this->preg_match_singlearg('!GCC ([\d\.]+)\s+?\(<a href="gcc-[\d\.]+/changes.html">changes</a>!s', $html);
+    	$html = $this->strcrop($html, 'Releases</h2>', '</dl>');
 
-	preg_match_all('/<dt><span class="version">.+?<\/span>/s', $html, $match);
+    	preg_match_all('/<dt><span class="version">.+?<\/span>/s', $html, $match);
 
-	$first = true;
-	foreach($match[0] as $ver) {
-		$vers = $this->preg_match_singlearg('!GCC ([\d\.]+)!', $ver);
-		#var_dump($ver);
-		if($first) {
-			$first = false;
-			$version_st = $vers;
-		}elseif(strpos(strtolower($ver), "development") !== false) {
-			$version_dv = $vers;
-		}elseif(!isset($version_os)){
-			$version_os = $vers;
-		}
-	}
+    	$first = true;
+    	foreach($match[0] as $ver) {
+    		$vers = $this->preg_match_singlearg('!GCC ([\d\.]+)!', $ver);
+    		var_dump($ver);
+    		if($first) {
+    			$first = false;
+    			$version_st = $vers;
+    		}elseif(strpos(strtolower($ver), "development") !== false) {
+    			$version_dv = $vers;
+    		}elseif(!isset($version_os)){
+    			$version_os = $vers;
+    		}
+    	}
 
         $download32 = 'http://gcc.gnu.org/';
 
-	#var_dump($version_st ,$version_os , $version_dv , $download32);
-#var_dump( $version_st, $version_os, $version_dv);
-#die();
-
-	if(!$version_st || !$version_os || !$download32) {
+	    if(!$version_st || !$version_os || !$download32) {
             $this->fail($this->meta_module);
         }else{
             $this->setversion(47, $version_st, $download32);
@@ -51,5 +43,3 @@ class PD_check_gcc extends PD_check {
 
 $check = new PD_check_gcc;
 $check->check();
-
-
